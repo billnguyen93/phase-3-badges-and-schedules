1046fbe125b5141a95b9354bcfaf46a7e6fa09cd
Bill Nguyen <billfl93@gmail.com>
2022-12-08 23:30:01 -0500

    commit tree b1c4c98c29d4395731518b83fdfaaa4b8b7b8194

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names_array)
+    names_array.map do |name|
+        "Hello, my name is #{name}."
+    end
+end
+
+def assign_rooms(speakers)
+    speakers.map.with_index(1) do |speaker, room_num|
+        "Hello, #{speaker}! You'll be assigned to room #{room_num}!"
+    end
+end
+
+def printer(speakers)
+    batch_badge_creator(speakers).each do |badge|
+        puts badge
+    end
+
+    assign_rooms(speakers).each do |assignment|
+        puts assignment
+    end
+end
